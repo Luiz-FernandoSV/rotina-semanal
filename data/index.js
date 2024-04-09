21ad7e8356bc4696e9f8bec6cac1b4d0d7fd2f02
unknown <luiz.fernandosv23@gmail.com>
2024-04-09 13:46:35 -0300

    Objetos com as tarefas adicionado

---
+++ data/index.js
@@ -0,0 +1,44 @@
+const DATA = [
+    {
+      "id": "1",
+      "porcentagem": "25%",
+      "imagem": uri('https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg'),
+      "titulo": "Estudar - 14h",
+      "descricao": "Estudar as diversas matérias da escola"
+    },
+    {
+      "id": "2",
+      "porcentagem": "100%",
+      "imagem": uri('https://st.focusedcollection.com/13422768/i/650/focused_160565312-stock-photo-satellite-view-of-earth.jpg'),
+      "titulo": "Lavar a louça - 4h",
+      "descricao": "Lavar a louça de casa"
+    },
+    {
+      "id": "3",
+      "porcentagem": "15%",
+      "imagem": uri('https://static.vecteezy.com/ti/fotos-gratis/t1/22653879-fantasia-ilha-com-cachoeiras-3d-ilustracao-elementos-do-isto-imagem-mobiliado-de-nasa-generativo-ai-gratis-foto.jpg'),
+      "titulo": "Ir no Mercado - 4h",
+      "descricao": "Tempo passado em idas ao mercado"
+    },
+    {
+      "id": "4",
+      "porcentagem": "45%",
+      "imagem": uri('https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-536613027-1024x683.jpg'),
+      "titulo": "Dormir - 63h",
+      "descricao": "Tempo gasto dormindo"
+    },
+    {
+      "id": "5",
+      "porcentagem": "68%",
+      "imagem": uri('https://png.pngtree.com/thumb_back/fw800/background/20230611/pngtree-wolf-animals-images-wallpaper-for-pc-384x480-image_2916211.jpg'),
+      "titulo": "Escola - 26h",
+      "descricao": "Tempo gasto na escola"
+    },
+    {
+      "id": "6",
+      "porcentagem": "48%",
+      "imagem": uri('https://i.kym-cdn.com/entries/icons/original/000/021/807/ig9OoyenpxqdCQyABmOQBZDI0duHk2QZZmWg2Hxd4ro.jpg'),
+      "titulo": "Tempo livre - 41h",
+      "descricao": "Tempo de lazer e recreação"
+    }
+]
